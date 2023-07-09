b0d06f2169760be987bb3da4ce976353bf53831d
Tylor Allison <tylor@foobox.com>
2023-07-09 17:58:56 -0500

    add util hashstr function

--- js/util.js
+++ js/util.js
@@ -52,6 +52,17 @@ class Util {
         }
     }
 
+    static hashStr(str) {
+        var hash = 0, i, chr;
+        if (str.length === 0) return hash;
+        for (let i=0; i<str.length; i++) {
+            chr = str.charCodeAt(i);
+            hash = ((hash << 5) - hash) + chr;
+            hash |= 0; // Convert to 32bit integer
+        }
+        return hash;
+    }
+
     static copy(entity, cache = new WeakMap()) {
         if (!entity) return undefined;
         if (cache.has(entity)) return cache.get(entity);
