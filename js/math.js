31bc82b865165e978ab05bb5de59caa272643137
Tylor Allison <tylor@foobox.com>
2023-08-14 07:12:36 -0500

    fix math bug

--- js/math.js
+++ js/math.js
@@ -9,12 +9,12 @@ class Mathf {
     }
 
     static clamp(val, min, max) {
-        return val > max ? max : val < min ? min : val;
+        return (val > max) ? max : ((val < min) ? min : val);
     }
 
     static clampInt(val, min, max) {
         val = parseInt(val);
-        return val > max ? max : val < min ? min : val;
+        return (val > max) ? max : ((val < min) ? min : val);
     }
 
     static roundTo(val, base=1) {
