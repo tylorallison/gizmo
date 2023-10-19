9f5e60c6a4c5e1ff04b2192ba9ba0df5683725a6
Tylor Allison <tylor@foobox.com>
2023-10-18 21:50:02 -0500

    timer: account for elapsed/ticks properly when timers trigger

--- js/timer.js
+++ js/timer.js
@@ -12,6 +12,7 @@ class Timer extends Gadget {
         this.schema('loop', { readonly: true, dflt: false });
         this.schema('cb', { readonly: true, dflt: () => false });
         this.schema('data', { readonly: true });
+        this.schema('ticks', { dflt: 0 });
     }
 
     constructor(spec={}) {
@@ -27,34 +28,41 @@ class Timer extends Gadget {
     onTock(evt) {
         Stats.count('timer.ontock');
         this.ttl -= evt.elapsed;
+        this.ticks += evt.ticks;
         if (this.ttl <= 0) {
+            let ticks = this.ticks;
             let overflow = -this.ttl;
             if (this.loop) {
                 this.ttl += this.startTTL;
+                this.ticks = 0;
                 if (this.ttl < 0) this.ttl = 0;
             } else {
                 Evts.ignore(null, 'GameTock', this.onTock, this);
             }
-            this.cb(Object.assign( { overflow: overflow, elapsed: this.startTTL + overflow }, evt, this.data));
+            this.cb(Object.assign( {}, evt, this.data, { ticks: ticks, overflow: overflow, elapsed: this.startTTL + overflow } ));
         }
     }
 
 }
 
 class Ticker extends Timer {
+    static { this.schema('elapsed', { dflt: 0 }); }
 
     onTock(evt) {
         Stats.count('ticker.ontock');
         this.ttl -= evt.ticks;
+        this.elapsed += evt.elapsed;
         if (this.ttl <= 0) {
+            let elapsed = this.elapsed;
             let overflow = -this.ttl;
             if (this.loop) {
+                this.elapsed = 0;
                 this.ttl += this.startTTL;
                 if (this.ttl < 0) this.ttl = 0;
             } else {
                 Evts.ignore(null, 'GameTock', this.onTock, this);
             }
-            this.cb(Object.assign( { overflow: overflow, elapsed: this.startTTL + overflow }, evt, this.data));
+            this.cb(Object.assign( {}, evt, this.data, { ticks: this.startTTL + overflow, overflow: overflow, elapsed: elapsed } ));
         }
     }
 }
